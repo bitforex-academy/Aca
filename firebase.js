bd98ecf47918dabf0132cf3697abd12a2960d124
BiTForeX Academy <arthurebube8@gmail.com>
2026-01-26 06:44:55 +0100

    Upgrade Firebase SDK to version 10.12.2

--- firebase.js
+++ firebase.js
@@ -1,23 +1,21 @@
 // firebase.js
-import { initializeApp } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-
-import { getAuth } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
-
-import { getFirestore } from
-"https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBhClpR0zHg4XYyTsDupLkmDIp_EkIzHEE",
   authDomain: "bitforex-academy-3a8f4.firebaseapp.com",
   projectId: "bitforex-academy-3a8f4",
-  storageBucket: "bitforex-academy-3a8f4.firebasestorage.app",
+  storageBucket: "bitforex-academy-3a8f4.appspot.com", // fixed!
   messagingSenderId: "659879098852",
   appId: "1:659879098852:web:16545b1980e2ed284a6ff1"
 };
 
 const app = initializeApp(firebaseConfig);
 
+// EXPORT everything app.js expects
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
